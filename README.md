1038ebd7f2f4050f95b38d69ad8a6399ab5ee4f4
Keheira <keheirah@gmail.com>
2015-10-11 08:48:34 -0400

    Last touches

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ##Secure Technology Exchange
+Application that allows students to find safe public places to make technology sells and trades. Also lessens the rist of scams by not allowing students to charge/pay each other until they meet in person and everything is agreed upon.
 
 ##Reference:
 <ul>
--- login.php
+++ login.php
@@ -1,9 +1,29 @@
 <?php
+<button>Login</button>
+$dbhost = $_SERVER["gotdb.cl5k3tw2xzvq.us-west-2.rds.amazonaws.com:3307"];
+$dbuser = $_SERVER["Teamflex"];
+$dbpass = $_SERVER["flexin4u"];
+$conn = mysql_connect($dbhost,$dbuser,$dbpass);
+
+   if(! $conn )
+   {
+      die('Could not connect: ' . mysql_error());
+   }
+   
+   echo 'Connected successfully';
+   
+   mysql_select_db( 'users' );
+
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$sql = "SELECT ".$username "FROM Users ()
-VALUES()";
+$sql = "SELECT * FROM Login WHERE Username=$username AND password = $password";
 
+$result = mysql_query($sql);
+
+if(!$result){
+    header("Location: buy.html");
+}
+    mysql_close($conn);
 ?>
